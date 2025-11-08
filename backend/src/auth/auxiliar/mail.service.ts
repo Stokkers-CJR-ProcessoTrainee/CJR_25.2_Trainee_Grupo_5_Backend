f6c6842a7f13eef9db0d128439f70fbbc16c0564
felipeab-b <felipeabferreira22@gmail.com>
2025-11-08 13:08:42 -0300

    test: console.log

--- backend/src/auth/auxiliar/mail.service.ts
+++ backend/src/auth/auxiliar/mail.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from "@nestjs/common";
 @Injectable()
 export class MailService {
   async sendResetCode(email: string, code: string) {
-    console.log(`Email enviado para ${email} com o código ${code}`);
+    console.log(`Email enviado para ${email} com o código ${code}, teste`);
   }
 }
