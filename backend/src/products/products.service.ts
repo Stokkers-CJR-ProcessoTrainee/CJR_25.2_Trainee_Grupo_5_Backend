2f601ec7044c861653f9d2d3277c7cad4b46fe99
Caio Cordeiro de Sousa <caiocord2005@gmail.com>
2025-12-05 00:40:56 -0300

    feat/add-delete-image-modal

--- backend/src/products/products.service.ts
+++ backend/src/products/products.service.ts
@@ -115,7 +115,7 @@ export class ProductsService {
   async findOne(id: number) {
     return await this.prisma.products.findFirst({
       where: { id: id },
-      include: { store: { select: { sticker_url: true, user_id: true } }, category: { select: { name: true } }, product_images: { select: { order: true, image_url: true } }, product_ratings: { select: { rating: true } } }
+      include: { store: { select: { sticker_url: true, user_id: true } }, category: { select: { name: true } }, product_images: { select: { id: true, order: true, image_url: true } }, product_ratings: { select: { rating: true } } }
     });
   }
 
